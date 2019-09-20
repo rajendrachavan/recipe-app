2272f5bc5a778974c35c3be0c2022fb61d8e2118
rajendrachavan <rajendrachavan.jvm@gmail.com>
2019-09-20 20:36:53 +0530

    Created methods to find description for category and Unit of measure.

--- src/main/java/spring5/kitchen/Controllers/IndexController.java
+++ src/main/java/spring5/kitchen/Controllers/IndexController.java
@@ -2,12 +2,33 @@ package spring5.kitchen.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring5.kitchen.Repositories.CategoryRepository;
+import spring5.kitchen.Repositories.UnitOfMeasureRepository;
+import spring5.kitchen.domain.Category;
+import spring5.kitchen.domain.UnitOfMeasure;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID is: "+categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure ID is: "+unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/spring5/kitchen/Repositories/UnitOfMeasureRepository.java
+++ src/main/java/spring5/kitchen/Repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package spring5.kitchen.Repositories;
 import org.springframework.data.repository.CrudRepository;
 import spring5.kitchen.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
