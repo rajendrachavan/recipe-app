2da988e9493393476bdb62e5cac89517f79a883d
rajendrachavan <rajendrachavan.jvm@gmail.com>
2019-09-20 13:33:47 +0530

    Fixed UnitOfMeasure Relationship Problem, and Created Category with ManyToMany Relationship with Recipe.

--- src/main/java/spring5/kitchen/domain/Category.java
+++ src/main/java/spring5/kitchen/domain/Category.java
@@ -4,12 +4,15 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-public class UnitOfMeasure {
+public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
 
     public Long getId() {
         return id;
@@ -25,5 +28,13 @@ public class UnitOfMeasure {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
     }
 }
